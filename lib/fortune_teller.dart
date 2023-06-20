1e40027770bbbaa75b3d4d16216c66b2e3b9ab6a
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-06-20 09:09:38 +0900

    占い師詳細画面に設計通りの要素を追加

--- lib/fortune_teller.dart
+++ lib/fortune_teller.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sampleapp_1/first_page.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const data = [
+    const _teller_card = [
       Text("占い師の名前"),
-      Text("占い師の一言説明"),
       Text("占い師のアイコン"),
-      Text("占術の説明")
+      Text("料金体系"),
+      Text("お気に入り追加ボタン")
+    ];
+    const _reviews = [
+      Text("レビューの合計数"),
+      Text("レビューの平均値(?)"),
+      Text("レビューの一覧ボタン"),
+      Text("一番いいレビューのテキスト"),
+    ];
+    const _basic_info = [Text("相談数"), Text("お気に入り数")];
+    const _detailed_info = [
+      Text("鑑定項目"),
+      Text("LLMからの占い師的一言"),
     ];
     return Scaffold(
       appBar: AppBar(title: const Text('占い師詳細画面を作るよ')),
-      body: Center(
-          child: Card(
-        child: ListView(children: data),
-      )),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: ListView(children: _teller_card),
+            ),
+            Expanded(
+              child: ListView(children: _reviews),
+            ),
+            Expanded(
+              child: ListView(children: _basic_info),
+            ),
+            Expanded(
+              child: ListView(children: _detailed_info),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => FirstPage()));
+              },
+              child: const Text('鑑定する'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
